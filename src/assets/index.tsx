fffda0563403ac691df77f20d4d2bd6f726dc00d
drizzy009 <yusid03@gmail.com>
2023-03-31 07:42:30 +0100

    Assets Update

---
+++ src/assets/index.tsx
@@ -0,0 +1,15 @@
+import person from './person.png';
+import person_1 from './person_1.png';
+import person_2 from './person_2.png';
+import person_3 from './person_3.png'
+import person_4 from './person_4.png'
+import person_5 from './person_5.png'
+import person_6 from './person_6.png'
+import person_7 from './person_7.png'
+import person_8 from './person_8.png'
+import person_9 from './person_9.png'
+import person_10 from './person_10.png'
+import person_11 from './person_11.png'
+import person_12 from './person_12.png'
+import techtime from './techtime.svg'
+
