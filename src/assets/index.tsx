6f717cd235df494480fdeff205dafed8f9685405
drizzy009 <yusid03@gmail.com>
2023-04-01 08:01:36 +0100

    asset update

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,15 +1,105 @@
-import person from './person.png';
-import person_1 from './person_1.png';
-import person_2 from './person_2.png';
-import person_3 from './person_3.png'
-import person_4 from './person_4.png'
-import person_5 from './person_5.png'
-import person_6 from './person_6.png'
-import person_7 from './person_7.png'
-import person_8 from './person_8.png'
-import person_9 from './person_9.png'
-import person_10 from './person_10.png'
-import person_11 from './person_11.png'
-import person_12 from './person_12.png'
-import techtime from './techtime.svg'
+import hero_image from "./hero_image.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import adam from "./adam.png";
+import jeff from "./jeff.png";
+import samuel from "./samuel.png";
+import leonard from "./leonard.png";
+import claretta from "./claretta.png";
+import jessica from "./jessica.png";
+import book from "./menu_book.svg";
+import schedule from "./schedule.svg";
+import person from "./person.png";
+import person0 from "./person_0.png";
+import person00 from "./person_00.png";
+import person_1 from "./person_1.png";
+import person_2 from "./person_2.png";
+import person_3 from "./person_3.png";
+import person_4 from "./person_4.png";
+import person_5 from "./person_5.png";
+import person_6 from "./person_6.png";
+import person_7 from "./person_7.png";
+import person_8 from "./person_8.png";
+import person_9 from "./person_9.png";
+import person_10 from "./person_10.png";
+import person_11 from "./person_11.png";
+import person_12 from "./person_12.png";
+import techtime from "./techtime.svg";
+import unsplash_1 from "./unsplash_4-EeTnaC1S4.png";
+import unsplash_2 from "./unsplash_6MePtA9EVDA.png";
+import unsplash_3 from "./unsplash_Lks7vei-eAg.png";
+import unsplash_4 from "./unsplash_Oalh2MojUuk.png";
+import unsplash_5 from "./unsplash_U33fHryBYBU.png";
+import unsplash_6 from "./unsplash_VtKoSy_XzNU.png";
+import unsplash_7 from "./unsplash_gMsnXqILjp4.png";
+import unsplash_8 from "./unsplash_pjAH2Ax4uWk.png";
+import unsplash_9 from "./unsplash_pypeCEaJeZY.png";
+import twitter from "./twitter.svg";
+import youtube from "./youtube.svg";
+import insta from "./insta.svg";
+import fb from "./fb.svg";
+import discord from "./discord.svg";
+import grade from "./grade.svg";
+import dollar from "./dollar.svg";
+import people from "./people.svg";
+import clock from "./clock.svg";
+import rss from "./rss.svg";
+import zoom from "./zoom.svg";
+import stripe from "./stripe.svg";
+import monday from "./monday.svg";
+import slack from "./slack.svg";
+import dropbox from "./dropbox.svg";
 
+export {
+  hero_image,
+  close,
+  zoom,
+  stripe,
+  slack,
+  monday,
+  dropbox,
+  menu,
+  person,
+  person_1,
+  person_2,
+  person_3,
+  person_4,
+  person_5,
+  person_6,
+  person_7,
+  person_8,
+  person_9,
+  person_10,
+  person_11,
+  person_12,
+  techtime,
+  adam,
+  jeff,
+  jessica,
+  samuel,
+  leonard,
+  claretta,
+  schedule,
+  book,
+  unsplash_1,
+  unsplash_2,
+  unsplash_3,
+  unsplash_4,
+  unsplash_5,
+  unsplash_6,
+  unsplash_7,
+  unsplash_8,
+  unsplash_9,
+  grade,
+  twitter,
+  youtube,
+  insta,
+  fb,
+  discord,
+  dollar,
+  people,
+  clock,
+  rss,
+  person0,
+  person00,
+};
